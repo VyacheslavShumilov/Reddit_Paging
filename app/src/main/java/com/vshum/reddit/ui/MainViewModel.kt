f70a7e706cf42e7ac0737987fdc75eaf07de111d
user.email <vvshumilov@mail.ru>
2023-01-26 11:12:20 +0300

    MainPostsMediator

--- app/src/main/java/com/vshum/reddit/ui/MainViewModel.kt
+++ app/src/main/java/com/vshum/reddit/ui/MainViewModel.kt
@@ -1,9 +1,25 @@
 package com.vshum.reddit.ui
 
-import androidx.paging.ExperimentalPagingApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.*
+import com.vshum.reddit.AppConstants
 import com.vshum.reddit.data.api.ApiRepository
 import com.vshum.reddit.data.db.CacheRepository
+import com.vshum.reddit.data.db.entities.PostData
+import com.vshum.reddit.ui.service.MainPostsMediator
 
 @ExperimentalPagingApi
-class MainViewModel(private val cacheRepository: CacheRepository, private val apiRepository: ApiRepository) {
+class MainViewModel(private val cacheRepository: CacheRepository, private val apiRepository: ApiRepository): ViewModel() {
+    /** Получить liveData по постам */
+    fun getPostsLiveData(): LiveData<PagingData<PostData>> {
+        val posts = {cacheRepository.getPostsBeforeDate()}
+        return Pager(config = getDefaultPageConfig(), pagingSourceFactory = posts, remoteMediator = MainPostsMediator(cacheRepository, apiRepository)).liveData
+    }
+
+    private fun getDefaultPageConfig(): PagingConfig {
+        return PagingConfig(pageSize = AppConstants.LIMIT_POSTS, enablePlaceholders = false)
+
+    }
+
 }
