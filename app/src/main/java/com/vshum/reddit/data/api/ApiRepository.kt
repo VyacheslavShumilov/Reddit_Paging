5b1cf3e0705cde95c49eed2d8dc187153063f2b8
user.email <vvshumilov@mail.ru>
2023-01-26 10:52:44 +0300

    api repo

---
+++ app/src/main/java/com/vshum/reddit/data/api/ApiRepository.kt
@@ -0,0 +1,15 @@
+package com.vshum.reddit.data.api
+
+import com.vshum.reddit.data.db.entities.PostData
+
+/** Интерфейс работы с API */
+interface ApiRepository {
+
+    /**
+     * Загрузить список постов.
+     * @param after уникальный id поста, после которого нужно получить новый список постов.
+     * @param before уникальный id поста, до которого нужно получить новый список постов.
+     * @return список постов [PostData] или null, если нет постов
+     */
+    suspend fun getPosts(after: String? = null, before: String? = null): List<PostData>
+}
--- app/src/main/java/com/vshum/reddit/ui/MainViewModel.kt
+++ app/src/main/java/com/vshum/reddit/ui/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.vshum.reddit.ui
 
 import androidx.paging.ExperimentalPagingApi
+import com.vshum.reddit.data.api.ApiRepository
+import com.vshum.reddit.data.db.CacheRepository
 
 @ExperimentalPagingApi
-class MainViewModel(private val cacheRepository: ) {
+class MainViewModel(private val cacheRepository: CacheRepository, private val apiRepository: ApiRepository) {
 }
